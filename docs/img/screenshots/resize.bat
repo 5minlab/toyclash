f2c008437ccae6bcc1a2384810751d1c267e927e
if1live <libsora25@gmail.com>
2017-01-18 21:05:39 +0900

    스샷 썸네일 크기 조정 대문 페이지 약간 조정

--- docs/img/screenshots/resize.bat
+++ docs/img/screenshots/resize.bat
@@ -1,9 +1,9 @@
 magick full_01.png -resize 720 resize_01.jpg
 
-magick full_02.png -resize 360 resize_02.jpg
-magick full_03.png -resize 360 resize_03.jpg
-magick full_04.png -resize 360 resize_04.jpg
-magick full_05.png -resize 360 resize_05.jpg
+magick full_02.png -resize 640 resize_02.jpg
+magick full_03.png -resize 640 resize_03.jpg
+magick full_04.png -resize 640 resize_04.jpg
+magick full_05.png -resize 640 resize_05.jpg
 
 magick full_01.png -resize 1920 resize_large_01.jpg
 magick full_02.png -resize 1920 resize_large_02.jpg
